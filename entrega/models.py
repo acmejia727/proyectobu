b3dc29d130c90552ded2a597dac3927f0750557c
acmejia727 <acmejia.xpe@gmail.com>
2019-08-26 17:43:23 -0500

    initial

---
+++ entrega/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Entrega(models.Model):
+    estado = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=100)
+    fecha_reg = models.DateTimeField(auto_now=True)
+    
---
+++ entrega/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import *
+# Create your views here.
+def home(request):
+    entregado = Entrega.objects.all()
+    context={'entregado':entregado}
+    return render(request, 'index.html', context)
